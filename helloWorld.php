77d9e2c1fa7bbb3375f965711036946458f4f6f4
n8thanael <n8thanael@gmail.com>
2016-04-04 11:01:45 -0400

    OK this seems to be working now....but I'm gonna break it soon I'm sure

--- helloWorld.php
+++ helloWorld.php
@@ -4,4 +4,6 @@ echo 'Hi there people<br>';
 
 echo 'Let\'s comit this change<br>';
 
-echo 'I think the file should be updating on the server<br>'
+echo 'I think the file should be updating on the server<br>';
+
+echo "<p>Does this even work?</p>";
